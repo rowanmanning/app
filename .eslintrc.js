48b7a1476b9f7e786a904d0e88e43c3088a786e6
Rowan Manning <accounts+github@rowanmanning.com>
2020-12-21 15:20:02 +0000

    Upgrade linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,5 @@
 'use strict';
 
 module.exports = {
-	extends: '@rowanmanning/eslint-config/es2017',
-	rules: {
-		'callback-return': 'off',
-		complexity: [
-			'warn',
-			{
-				max: 10
-			}
-		],
-		'max-statements': [
-			'warn',
-			{
-				max: 35
-			}
-		]
-	}
+	extends: '@rowanmanning/eslint-config'
 };
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const config = module.exports = JSON.parse(JSON.stringify(require('../.eslintrc')));
-
-config.rules['max-len'] = 'off';
-config.rules['max-statements'] = 'off';
+module.exports = {
+	extends: '../.eslintrc',
+	rules: {
+		'max-len': 'off'
+	}
+};
